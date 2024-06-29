d22144310ba004d2454c6446f5579530ef4cf999
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2024-06-29 00:56:42 +0000

    working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  components: [{
+    path: "~/src/components"
+  }],
+  devServer: {
+    port: 7777
+  }
+  
 })
